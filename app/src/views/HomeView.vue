a28c5a197ae3ee4ee9036aca7b3b9f798054cc29
kongeebowl <kadenp11@nycstudents.net>
2025-03-04 12:30:26 -0500

    i hate this

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,5 +1,27 @@
-<script setup></script>
-
 <template>
-  <main></main>
+  <div class="container">
+    <PokemonCards
+      v-for="(pkmn, index) in pokemon.results"
+      :key="index"
+      :pokemon="pkmn"
+      :id="index + 1"
+    >
+    </PokemonCards>
+  </div>
 </template>
+<script setup>
+import { ref, onMounted } from 'vue'
+import PokemonCards from '../components/PokemonCards.vue'
+const pokemon = ref('')
+
+async function getPokemon() {
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=151')
+  let data = await response.json()
+  pokemon.value = data
+}
+onMounted(() => {
+  getPokemon()
+})
+</script>
+
+<style scoped></style>
