a640e95bb67a4958a7487c05acae20f91ebe303c
kongeebowl <kadenp11@nycstudents.net>
2025-03-17 12:32:17 -0400

    ewwea

--- app/src/components/TheHeader.vue
+++ app/src/components/TheHeader.vue
@@ -2,8 +2,14 @@
   <div>
     <header class="text-center m-auto">
       <h1 class="text-3xl my-8 m-auto text-[#deb363]">Crimes in New York City</h1>
+      <p class="text-xs text-[#3d3637]">(well well well...)</p>
     </header>
-    <nav class="container m-auto flex justify-center">
+    <nav class="container m-auto flex justify-center my-5">
+      <router-link
+        to="/"
+        class="text-[#fff8a5] m-auto dark:md:hover:bg-yellow-600 transition duration-300"
+        >Home</router-link
+      >
       <router-link
         to="skincolor"
         class="text-[#fff8a5] m-auto dark:md:hover:bg-yellow-600 transition duration-300"
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="container">
-    <div v-for="(person, index) in people" :key="index" :person="person" :id="index + 1">
+  <div class="container items-center justify-center flex flex-col m-auto">
+    <div
+      v-for="(person, index) in people"
+      :key="index"
+      :person="person"
+      :id="index + 1"
+      class="card w-full sm:w-[90%] md:w-[80%] lg:w-[75%] h-auto rounded-3xl flex flex-col items-center justify-between bg-blue-200 text-center p-2 shadow-sm m-5"
+    >
+      {{ perp }}
       {{ person.pd_desc }}
     </div>
   </div>
