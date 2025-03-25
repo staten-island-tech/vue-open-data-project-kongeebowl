128c01c9105fb70a7362aff5ea73ef5e2326c690
kongeebowl1 <kadenpang09@gmail.com>
2025-03-25 19:46:42 -0400

    e

--- app/src/components/PrintCards.vue
+++ app/src/components/PrintCards.vue
@@ -4,7 +4,7 @@
       {{ person.perp_race }}
       {{ person.pd_desc }}
       <img
-        src="https://dims.apnews.com/dims4/default/d568bd0/2147483647/strip/true/crop/1621x1080+149+0/resize/599x399!/quality/90/?url=https%3A%2F%2Fassets.apnews.com%2F61%2F10%2Fc02e5570477abbc1d969e97bfece%2Funtitled-design-4.png"
+        src="https://static.wikia.nocookie.net/nopixel/images/e/ec/Timcheese.png.png/revision/latest?cb=20250305065848"
         alt="trumpy"
       />
       {{ person.perp_sex }}
