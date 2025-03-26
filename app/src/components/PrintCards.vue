a28c8c422f468c4256177cfc1a9f36cc23526dea
kongeebowl <kadenpang09@gmail.com>
2025-03-26 09:35:14 -0400

    Update PrintCards.vue

--- app/src/components/PrintCards.vue
+++ app/src/components/PrintCards.vue
@@ -4,7 +4,7 @@
       {{ person.perp_race }}
       {{ person.pd_desc }}
       <img
-        src="https://static.wikia.nocookie.net/nopixel/images/e/ec/Timcheese.png.png/revision/latest?cb=20250305065848"
+        src="https://static.wikia.nocookie.net/nopixel/images/d/da/Timycheese.png.png/revision/latest/scale-to-width-down/250?cb=20250305081122"
         alt="trumpy"
       />
       {{ person.perp_sex }}
