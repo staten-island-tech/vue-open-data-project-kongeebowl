5d7d266827bf88e15cfe0a9caed03c6dcc226bd2
kongeebowl <kadenpang09@gmail.com>
2025-03-26 09:49:26 -0400

    Update PrintCards.vue

--- app/src/components/PrintCards.vue
+++ app/src/components/PrintCards.vue
@@ -4,7 +4,7 @@
       {{ person.perp_race }}
       {{ person.pd_desc }}
       <img
-        src="https://www.google.com/imgres?q=Jamarius%20Quangle-Dangle&imgurl=https%3A%2F%2Fstatic.wikia.nocookie.net%2Famongusfan22%2Fimages%2Fa%2Fa3%2F6th-grader-leader-current.png%2Frevision%2Flatest%2Fthumbnail%2Fwidth%2F360%2Fheight%2F360%3Fcb%3D20231113021343&imgrefurl=https%3A%2F%2Famongusfan22.fandom.com%2Fwiki%2FJamarius_Quangledangle&docid=ZLYnFEEO5W_mwM&tbnid=tasHIbM5KdkpaM&vet=12ahUKEwjPzZCv76eMAxXjg4kEHdsxHTUQM3oECBMQAA..i&w=200&h=360&hcb=2&ved=2ahUKEwjPzZCv76eMAxXjg4kEHdsxHTUQM3oECBMQAA"
+        src="https://m.media-amazon.com/images/I/714-7Eu91wL._AC_UF1000,1000_QL80_.jpg"
         alt="trumpy"
       />
       {{ person.perp_sex }}
